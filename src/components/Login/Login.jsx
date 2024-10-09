f25be97626d7e927ddfd6d05ffd80115fd76a135
Youssef Tawfik <youssef.k.tawfik@gmail.com>
2024-10-09 14:28:53 +0300

    centering looading

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -92,7 +92,7 @@ export default function Login() {
           type="submit"
           className="w-full text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm  px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-500 dark:focus:ring-green-800"
         >
-          {isLoading ? <FaSpinner className="text-xl animate-spin" /> : "Login"}
+          {isLoading ? <FaSpinner className="text-xl animate-spin mx-auto" /> : "Login"}
         </button>
         <div>{loginError}</div>
         <button
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -117,7 +117,7 @@ export default function Register() {
           className="bg-green-500 hover:bg-green-800 text-white mt-5 border block focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-500 dark:focus:ring-green-800"
         >
           {isLoading ? (
-            <FaSpinner className="text-xl animate-spin" />
+            <FaSpinner className="text-xl animate-spin mx-auto" />
           ) : (
             "Register"
           )}

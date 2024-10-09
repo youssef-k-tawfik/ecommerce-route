22e2a83258aec3a7b0dac513e68895c8117895ae
Youssef Tawfik <youssef.k.tawfik@gmail.com>
2024-10-09 13:25:39 +0300

    tzbeet navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import cart from "../../assets/cart.png";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import { FaShoppingCart } from "react-icons/fa";
 import { CartContext } from "../../context/CartContext";
@@ -10,6 +10,31 @@ export default function Navbar() {
   const navigate = useNavigate();
   const { numberOfCartItems } = useContext(CartContext);
 
+  const [isNavBarVisible, setIsNavBarVisible] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setIsNavBarVisible(true);
+      } else {
+        setIsNavBarVisible(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Initial check
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function handleNavBarVisibility() {
+    setIsNavBarVisible(!isNavBarVisible);
+  }
+
   function signOut() {
     setToken(null);
     navigate("/login");
@@ -17,19 +42,22 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/" className="flex items-center md:space-x-3 rtl:space-x-reverse">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1 md:p-4">
+        <Link
+          to="/"
+          className="flex items-center md:space-x-3 rtl:space-x-reverse"
+        >
           <img src={cart} className="h-8" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             FreshCart
           </span>
         </Link>
-        <div className="flex md:order-2 spaasdce-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div className="flex md:order-2 md:space-x-0 rtl:space-x-reverse">
           <ul className="flex gap-1 md:gap-6 font-medium me-2 items-center">
             {token ? (
               <>
                 <li
-                  className="relative cursor-pointer"
+                  className="relative cursor-pointer me-2"
                   onClick={() => navigate("/cart")}
                 >
                   <FaShoppingCart className="text-2xl text-[#323232]" />
@@ -76,9 +104,10 @@ export default function Navbar() {
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2  dark:text-gray-400 dark:hover:bg-gray-500 "
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden  focus:outline-none focus:ring-2  dark:text-gray-400 dark:hover:bg-gray-500 "
             aria-controls="navbar-sticky"
             aria-expanded="false"
+            onClick={handleNavBarVisibility}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -99,62 +128,49 @@ export default function Navbar() {
           </button>
         </div>
         <div className="items-center justify-between  w-full flex lg:w-auto order-2 lg:order-1">
-          {token || true && (
-            <ul
-              className="flex flex-col w-full p-4 lg:p-0 mt-4 font-medium text-xl border border-gray-100 rounded-lg bg-gray-50 lg:space-x-8 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-500"
-              id="navbar-sticky"
-            >
-              <li>
-                <NavLink
-                  to=""
-                  className="block py-2 px-3 text-gray-900 rounded lg:bg-transparent   lg:p-0 lg:dark:text-white lg:dark:hover:text-green-500"
-                  aria-current="page"
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="cart"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent   lg:p-0 lg:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-500"
-                >
-                  Cart
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="wishlist"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent   lg:p-0 lg:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-500"
-                >
-                  Wishlist
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="products"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent   lg:p-0 lg:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-500"
-                >
-                  Products
-                </NavLink>
-              </li>
-              {/* <li>
-                <NavLink
-                  to="categories"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent   lg:p-0 lg:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-500"
-                >
-                  Categories
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="brands"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent   lg:p-0 lg:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-500"
-                >
-                  Brands
-                </NavLink>
-              </li> */}
-            </ul>
-          )}
+          <ul
+            className={`flex flex-col w-full p-4 lg:p-0 mt-4 font-medium text-xl border border-gray-100 rounded-lg bg-gray-50 lg:space-x-8 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-500 ${
+              isNavBarVisible ? " " : "hidden"
+            }`}
+          >
+            <li>
+              <NavLink
+                to=""
+                className="block py-2 px-3 text-gray-900 rounded lg:bg-transparent   lg:p-0 lg:dark:text-white lg:dark:hover:text-green-500"
+                aria-current="page"
+                onClick={() => setIsNavBarVisible(false)}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="cart"
+                className="block py-2 px-3 text-gray-900 rounded  lg:hover:bg-transparent   lg:p-0 lg:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-500"
+                onClick={() => setIsNavBarVisible(false)}
+              >
+                Cart
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="wishlist"
+                className="block py-2 px-3 text-gray-900 rounded  lg:hover:bg-transparent   lg:p-0 lg:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-500"
+                onClick={() => setIsNavBarVisible(false)}
+              >
+                Wishlist
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="products"
+                className="block py-2 px-3 text-gray-900 rounded  lg:hover:bg-transparent   lg:p-0 lg:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-500"
+                onClick={() => setIsNavBarVisible(false)}
+              >
+                Products
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
